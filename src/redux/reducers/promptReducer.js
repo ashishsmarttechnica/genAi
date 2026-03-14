25934c33b946664918d7d0854dd33ba9d4f38480
Hetvi Smarttechnica <monika.smarttechnica@gmail.com>
2026-03-14 15:26:55 +0530

    solve duplicate api calling on user list

--- src/redux/reducers/promptReducer.js
+++ src/redux/reducers/promptReducer.js
@@ -60,6 +60,7 @@ const PromptReducer = (state = initialState, action) => {
                 }
             });
 
+            
             return {
                 ...state,
                 loading: false,
